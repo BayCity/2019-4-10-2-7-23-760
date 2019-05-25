440f57153a3a9539d379e79702b1c20e144c0df3
BayCity <984851056@qq.com>
2019-05-26 00:24:51 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var cost;
+    if(0<inputs.distance&&inputs.distance<=2){
+        cost=6+inputs.parkTime*0.25;
+    }
+    else if(2<inputs.distance&&inputs.distance<=8){
+        cost=6+(inputs.distance-2)*0.8+inputs.parkTime*0.25;
+    }
+
+    else if(inputs.distance>8){
+        cost=10.8+(inputs.distance-8)*1.2+inputs.parkTime*0.25;
+    }
+    return Math.round(cost);
 };
